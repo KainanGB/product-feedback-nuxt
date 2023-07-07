5b98be9033b7f25c7e8f431278c37ae024f07a02
KainanGB <kainanytbr@gmail.com>
2023-07-07 00:03:03 -0300

    feat: redone filter options menu

--- components/FilterOptionsMenu.vue
+++ components/FilterOptionsMenu.vue
@@ -1,30 +1,41 @@
 <template>
-	<div class="nav-footer">
-		<q-btn-dropdown class="drop-btn" :menu-offset="[8, 20]" size="md" unelevated no-caps>
-			<template v-slot:label>
-				<div class="text-caption">
-					Sort By: <b>{{ selectedOpt }}</b>
-				</div>
-			</template>
-			<q-list>
-				<q-item
-					class="no-hover"
-					clickable
-					v-close-popup
-					v-for="option in filterOptions"
-					@click="selectedOpt = option.name"
-				>
-					<q-item-section>
-						<q-item-label>{{ option.name }}</q-item-label>
-					</q-item-section>
-				</q-item>
-			</q-list>
-		</q-btn-dropdown>
-		<q-btn label="+ Add feedback" rounded class="feedback-btn" unelevated no-caps></q-btn>
+	<div class="filter-options">
+		<div class="filter-button" @click="onToggleOptions">
+			<p>
+				Sort By: <b>{{ selectedOpt }}</b>
+			</p>
+			<Icon class="icon" :name="toggleOptions ? 'down' : 'up'" size="23" />
+		</div>
+		<transition-group
+			appear
+			enter-active-class="animated fadeIn"
+			leave-active-class="animated fadeOut"
+		>
+			<ul class="dropdown-menu" v-show="toggleOptions" key="dropdown-menu">
+				<li v-for="option in filterOptions" @click="setSelectOption(option.name)">
+					{{ option.name }}
+					<Icon
+						v-show="selectedOpt === option.name"
+						class="check-icon"
+						name="uil:check"
+						size="23"
+					/>
+				</li>
+			</ul>
+		</transition-group>
+		<NuxtLink to="/new-feedback">
+			<button class="feedback-btn">+ Add feedback</button>
+		</NuxtLink>
 	</div>
 </template>
 
 <script setup lang="ts">
+const toggleOptions = ref(false);
+
+function onToggleOptions(): void {
+	toggleOptions.value = !toggleOptions.value;
+}
+
 const selectedOpt = ref("Most Upvotes");
 const filterOptions = ref([
 	{
@@ -40,6 +51,11 @@ const filterOptions = ref([
 		name: "Least Comments",
 	},
 ]);
+
+function setSelectOption(option: string): void {
+	selectedOpt.value = option;
+	toggleOptions.value = false;
+}
 </script>
 
 <style lang="scss" scoped>
